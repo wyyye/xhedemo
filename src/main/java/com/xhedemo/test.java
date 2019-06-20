5703a18b7a040592dde42672310f11bb7c4cafdd
Administrator <Administrator@7MUIEJQK9TIUTII>
2019-06-20 09:28:29 +0800

    define integer a.

--- src/main/java/com/xhedemo/test.java
+++ src/main/java/com/xhedemo/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("-------测试--------");
-		
+		int a=10;
 		
 		
 		System.out.println();
